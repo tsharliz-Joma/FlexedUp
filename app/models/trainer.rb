b1366210c503ddea0f6673d3c363da8dfb78bda3
Charles Joma <almprincecharles@yahoo.com>
2022-07-29 10:57:51 +1000

    2 quick bug fixes, presentation version complete

--- app/models/trainer.rb
+++ app/models/trainer.rb
@@ -1,6 +1,4 @@
 class Trainer < ApplicationRecord 
     has_many :bookings
-    # has_many :users # This is saying that a trainer has many users, meaning you can assign users to the trainer 
-    # has_many :bookings # This is saying that the trainer has many bookings, meaning you can assign bookings to the trainer 
 end
   
